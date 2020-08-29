1e4a97e5d72df7a8e6ee67f20b7a8b7d3e8ee5b6
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-08-29 11:48:42 +0200

    lesson 2

---
+++ src/apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    client: {
+        service: {
+            name: "spacex-graphql-api",
+            url: "https://api.spacex.land/graphql"
+        },
+        includes: ["src/**/*.vue", "src/**/*.js"]
+    }
+}
