dd470fc56a640659f27b3592210df0ff4253b2fd
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-08-29 09:58:53 +0200

    lesson 1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueApollo from 'vue-apollo'
+import Apolloclient from 'apollo-boost'
 
+const apolloClient = new Apolloclient({
+  uri: 'https://api.spacex.land/graphql'
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
+Vue.use(VueApollo)
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
